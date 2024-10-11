3a17f2e624f0470bd00dd1af81459b8769217516
Sidorychev Nickolai <sidorichev@tochka.com>
2024-10-11 11:33:01 +0300

    Добавил 2 класса мейн и мейн2 на уроке 1

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+public class Main {
+
+
+    //status file in git
+    // not tracked - красный
+    // added(созданный) - зеленый
+    // removed
+    // changed - синим
+
+    public static void main(String[] args) {
+
+        String colorDog  = "red";
+        int color2 = getSum(10, 15);
+        int qwe2 = getSum(16, 15);
+        System.out.println(color2);
+        System.out.println(qwe2);
+        printSum(10, 12);
+
+     }
+    //   public static возвращаемый тип имя метода (параметры) {}
+    public static int getSum(int x, int y){
+        int result;
+        result = x + y;
+        return result;
+    }
+
+    public static void printSum(int x, int y){
+        int result;
+        result = x + y;
+        System.out.println(result);
+        // sendMessageToEmailBox
+        // saveDataToDB
+        // sout
+    }
+
+
+
+// Примитивами
+// целые чиселки - byte, short, int ,long
+// дробные - float, double
+// логический - boolean
+// символ - char 'a'
+
+
+// Ссылочный тип данных
+    // строки - String - "qwe" - ['q','w','e']
+
+
+
+}
